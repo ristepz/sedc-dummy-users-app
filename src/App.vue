27e1b3bdd23708d8983da2acbf154262573ce05e
Riste Pazarkoski <r.pazarkoski@btobet.com>
2020-10-28 18:36:41 +0100

    UserForm component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
 <template>
-  <div>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+        <h4 class="page-title display-4">Dummy Users App</h4>
+      </div>
+    </div>
+    <div class="row main-row">
+      <UserForm />
+      <UserList />
+      <UserCard />
+    </div>
   </div>
 </template>
 
 <script>
+import UserForm from './components/UserForm.vue';
+import UserList from './components/UserList.vue';
+import UserCard from "./components/UserCard.vue";
 
 export default {
   name: 'App',
+  components:{
+    UserForm,
+    UserList,
+    UserCard
+  }
 }
 </script>
